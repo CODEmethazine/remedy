46dab14a21a5b3d9187b75b6b2f27bc69b44349e
SobStorie <flamearrowz@gmail.com>
2023-10-28 22:55:52 -0400

    home page

--- frontend/remedy/src/pages/index.js
+++ frontend/remedy/src/pages/index.js
@@ -1,14 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.css'; // Import the specific CSS file
 
-import React from "react";
- 
 const Home = () => {
-    return (
-        <div>
-            <h1>
-                Landing Page. Set meds view schedule
-            </h1>
-        </div>
-    );
+  const userName = "Rushi"; // Placeholder name
+
+  return (
+    <div className="homepage-body">
+      <div className="homepage-welcome-text">
+        Welcome, <span className="homepage-username">{userName}</span>
+      </div>
+      <div className="homepage-container">
+        <Link to="/setmeds" className="homepage-clickable-box">Set Meds</Link>
+        <Link to="/schedule" className="homepage-clickable-box">View Schedule</Link>
+      </div>
+    </div>
+  );
 };
- 
+
 export default Home;
