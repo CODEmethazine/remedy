5b3ef52e4c63481f5b629f7944c1f0939ea1a4ac
Nathanael Bekele <natibekele@Nathanaels-MacBook-Air-2.local>
2023-10-29 07:37:54 -0500

    confirm fronttt

--- frontend/remedy/src/pages/confirmation.js
+++ frontend/remedy/src/pages/confirmation.js
@@ -1,8 +1,33 @@
 import React from 'react';
- 
+import './confirmation.css';
+
 const Confirmation = () => {
     return (
-        <h1>Confirmation info</h1>
+        <body>
+        <h1>Did you take your medication today?</h1>
+        <div class="medication-details">
+            <div class="medication-field">
+                <div class="label">Name of Medication</div>
+                <div class="value">Chicken</div>
+            </div>
+            <div class="medication-field">
+                <div class="label">Dosage</div>
+                <div class="value">Chicken</div>
+            </div>
+            <div class="medication-field">
+                <div class="label">Frequency</div>
+                <div class="value">Chicken</div>
+            </div>
+            <div class="medication-field">
+                <div class="label">Notes</div>
+                <div class="value">Chicken</div>
+            </div>
+        </div>
+        <div class="action-buttons">
+            <button class="delete-button">No X</button>
+            <button class="checkmark-button">Yes &#10003;</button>
+        </div>
+    </body>
     );
 };
  
