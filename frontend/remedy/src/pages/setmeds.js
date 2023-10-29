f6ee7abb0abdc233041555b7755bf3540b035ea6
Nathanael Bekele <natibekele@Nathanaels-MacBook-Air-2.local>
2023-10-29 06:30:56 -0500

    setmeds front

--- frontend/remedy/src/pages/setmeds.js
+++ frontend/remedy/src/pages/setmeds.js
@@ -1,11 +1,34 @@
 
 import React from 'react';
+import './setmeds.css';
  
 const SetMeds = () => {
     return (
-        <div>
-            <h1>Set Meds</h1>
-        </div>
+        <body>
+    
+    <form action="/submit" method="post">
+        <label for="startDate">Start Date:</label>
+        <input type="date" id="startDate" name="startDate" required/><br></br>
+
+        <label for="medicationName">Name of the Medication:</label>
+        <input type="text" id="medicationName" name="medicationName" required/><br></br>
+
+        <label for="dosage">Dosage:</label>
+        <input type="text" id="dosage" name="dosage" required/><br></br>
+
+        <label for="frequency">Frequency:</label>
+        <input type="text" id="frequency" name="frequency" required/><br></br>
+
+        <label for="phoneNumber">Phone Number:</label>
+        <input type="tel" id="phoneNumber" name="phoneNumber" pattern="[0-9]{10}" required/><br></br>
+
+        <label for="notes">Notes:</label><br></br>
+        <textarea id="notes" name="notes" rows="4" cols="50"></textarea><br></br>
+
+        <input type="submit" value="Submit"/>
+    </form>
+</body>
+
     );
 };
  
