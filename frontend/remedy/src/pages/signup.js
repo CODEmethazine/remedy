b950af66ef547a5a2686285771b9414b05a3c178
natedoesthings <nathanaeldtesfaye@gmail.com>
2023-10-29 06:02:57 -0500

    final signup

--- frontend/remedy/src/pages/signup.js
+++ frontend/remedy/src/pages/signup.js
@@ -37,26 +37,7 @@ const SignUp = () => {
           </form>
         </div>
       </div>
-      <script>
-        let signupBtn = document.getElementById("signupBtn");
-        let signinBtn = document.getElementById("signinBtn");
-        let nameField = document.getElementById("nameField");
-        let title = document.getElementById("title");
-
-        signinBtn.onclick = function(){
-            nameField.style.maxHeight = "0";
-            title.innerHTML = "Log in";
-            signupBtn.classList.add("disable");
-            signinBtn.classList.remove("disable");
-        }
-
-        signupBtn.onclick = function(){
-            nameField.style.maxHeight = "60px";
-            title.innerHTML = "Sign Up";
-            signupBtn.classList.remove("disable");
-            signinBtn.classList.add("disable");
-        }
-      </script>
+      <script src="singupJS.js"></script>
     </body>
 
   );
