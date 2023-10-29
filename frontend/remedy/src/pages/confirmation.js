4f52609cd48ee8da4703389985b0419a8e0a29ff
natedoesthings <nathanaeldtesfaye@gmail.com>
2023-10-29 12:06:21 -0500

    final edits

--- frontend/remedy/src/pages/confirmation.js
+++ frontend/remedy/src/pages/confirmation.js
@@ -8,19 +8,19 @@ const Confirmation = () => {
         <div class="medication-details">
             <div class="medication-field">
                 <div class="label">Name of Medication</div>
-                <div class="value">Chicken</div>
+                <div class="value">CODEmethazine...</div>
             </div>
             <div class="medication-field">
                 <div class="label">Dosage</div>
-                <div class="value">Chicken</div>
+                <div class="value">30 ml...</div>
             </div>
             <div class="medication-field">
                 <div class="label">Frequency</div>
-                <div class="value">Chicken</div>
+                <div class="value">Every 12-16 hours...</div>
             </div>
             <div class="medication-field">
                 <div class="label">Notes</div>
-                <div class="value">Chicken</div>
+                <div class="value">No alcohol is allowed when taking this prescription...</div>
             </div>
         </div>
         <div class="action-buttons">
