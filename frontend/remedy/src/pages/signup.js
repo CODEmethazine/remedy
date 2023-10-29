197b14fc639a41941eb39c496159b9a282c01c1f
natedoesthings <nathanaeldtesfaye@gmail.com>
2023-10-29 06:10:13 -0500

    fixed signup

--- frontend/remedy/src/pages/signup.js
+++ frontend/remedy/src/pages/signup.js
@@ -3,6 +3,25 @@ import './signup.css';
 
 
 const SignUp = () => {
+  let signupBtn = document.getElementById("signupBtn");
+  let signinBtn = document.getElementById("signinBtn");
+  let nameField = document.getElementById("nameField");
+  let title = document.getElementById("title");
+
+  signinBtn.onclick = function () {
+    nameField.style.maxHeight = "0";
+    title.innerHTML = "Log in";
+    signupBtn.classList.add("disable");
+    signinBtn.classList.remove("disable");
+  }
+
+  signupBtn.onclick = function () {
+    nameField.style.maxHeight = "60px";
+    title.innerHTML = "Sign Up";
+    signupBtn.classList.remove("disable");
+    signinBtn.classList.add("disable");
+  }
+
   return (
     <body>
       <div class="container">
@@ -42,7 +61,7 @@ const SignUp = () => {
 
   );
 
-  
+
 };
 
 export default SignUp;
